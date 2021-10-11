ea4de3bf592538c0d1b3930a9f7cc607e52a657a
BakerrAbdelkareemm <bbaakkrrr97@gmail.com>
2021-10-11 17:24:02 +0300

    login-Process-Test

--- cypress/integration/my_first_test.js
+++ cypress/integration/my_first_test.js
@@ -1,40 +1,20 @@
 /// <reference types ="cypress" />
 
-
-/*
-it ('google test',function(){
-
-    cy.visit('https://google.com')
-    cy.get('.gLFyf').type('Automation step by step{enter}')
-  //cy.get('[href="https://www.youtube.com/channel/UCTt7pyY-o0eltq14glaG5dg"] > .LC20lb > span').click()
-  //cy.wait(1000)
-  //cy.contains('فيديو').click()
-})
-*/
 it ('login test',function(){
 
-    cy.visit('https://demo.realworld.io/?fbclid=IwAR3DDj55wxw_ip5DwIFQGo4N2s7pQH27vuRfpHkFnd4l_erwueQqiHsrWZI#/login')
-    cy.get(':nth-child(2) > .form-control').type('bakerbaker@gmail.com')
-    cy.get(':nth-child(3) > .form-control').type('123456789')
+    cy.visit('https://demo.realworld.io/?fbclid=IwAR3DDj55wxw_ip5DwIFQGo4N2s7pQH27vuRfpHkFnd4l_erwueQqiHsrWZI#/register')
+    cy.get('[placeholder=Username]').type('bakeeeerrr')
+    cy.get('[placeholder=Email]').type('bakeeeeerrrr@gmail.com')
+    cy.get('[placeholder=Password]').type('123456789')
+    cy.get('.btn').click()
+    cy.get('[show-authed="true"] > :nth-child(3) > .nav-link').click()
+    cy.get('.btn-outline-danger').click()
+    cy.get('[show-authed="false"] > :nth-child(3) > .nav-link').click()
+    cy.get('[placeholder=Username]').type('bakeeeerrr')
+    cy.get('[placeholder=Email]').type('bakeeeeerrrr@gmail.com')
+    cy.get('[placeholder=Password]').type('123456789')
     cy.get('.btn').click()
 
-})
 
-it ('login test',function(){
-
-    cy.visit('https://demo.realworld.io/?fbclid=IwAR3DDj55wxw_ip5DwIFQGo4N2s7pQH27vuRfpHkFnd4l_erwueQqiHsrWZI#/login')
-    cy.get(':nth-child(2) > .form-control').type('bakerbaker@gmail.com')
-    cy.get(':nth-child(3) > .form-control').type('12345')
-    cy.get('.btn').click()
 
 })
-
-it ('login test',function(){
-
-    cy.visit('https://demo.realworld.io/?fbclid=IwAR3DDj55wxw_ip5DwIFQGo4N2s7pQH27vuRfpHkFnd4l_erwueQqiHsrWZI#/login')
-    cy.get(':nth-child(2) > .form-control').type('b@gmail.com')
-    cy.get(':nth-child(3) > .form-control').type('123456789')
-    cy.get('.btn').click()
-
-})
-
